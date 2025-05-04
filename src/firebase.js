3b2882b9604f70c531f428dc648cdc3f4aecdd8d
Dennis-HIGH <dhidalgo1014@gmail.com>
2025-05-03 20:15:03 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0wfHCZqIC-Cfc9r3QfknVD0uyEtMRzlg",
-  authDomain: "login-fc8b6.firebaseapp.com",
-  projectId: "login-fc8b6",
+  apiKey: "AIzaSyDklfu5QrtTpOYCSurxDL2uhHsHkyEZbYg",
+  authDomain: "login-87f48.firebaseapp.com",
+  projectId: "login-87f48",
   storageBucket: "login-fc8b6.firebasestorage.app",
-  messagingSenderId: "84088347751",
+  messagingSenderId: "73435636260",
   appId: "1:84088347751:web:d95f0b9a34fe4af67248c2",
   measurementId: "G-QH71PQG7LH"
 };
